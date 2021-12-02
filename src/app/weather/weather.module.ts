72ed3783a0b0006bc29f1771b4264404040934e8
blaise2s <blaise.schaeffer@gmail.com>
2021-12-02 01:36:25 -0500

    feat: add buy button on weather wardrobe card
    Resolves: #WW-0001

--- src/app/weather/weather.module.ts
+++ src/app/weather/weather.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { WeatherComponent } from './weather.component';
@@ -7,6 +8,11 @@ import { WeatherComponent } from './weather.component';
 @NgModule({
   declarations: [WeatherComponent],
   exports: [WeatherComponent],
-  imports: [CommonModule, MatProgressSpinnerModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
 })
 export class WeatherModule {}
