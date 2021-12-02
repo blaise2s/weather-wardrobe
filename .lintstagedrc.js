feb664b4449782562edfa7a22c32e4d856676258
blaise2s <blaise.schaeffer@gmail.com>
2021-12-02 01:18:05 -0500

    chore: update lint staged configuration
    Resolves: #WW-0000

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  // '**/*.{ts,js}': 'eslint --cache --fix',
+  '**/*.{ts,js}': 'eslint --cache --fix',
   '**/*.scss': 'stylelint --fix',
   '**/*.{ts,js,scss,html,json,md,yml,yaml}':
     'prettier --write --ignore-unknown',
